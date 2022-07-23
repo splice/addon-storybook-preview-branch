ed5c70a79a0b8c506f4386e661a7697175fef454
David Branagh-Miscampbell <dave@stormideas.com>
2022-07-23 23:10:04 +0100

    [publish-dist] Publishing the dist folder

---
+++ dist/constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EVENTS = exports.ADDON_ID = void 0;
+exports.ADDON_ID = 'addon-preview-branch';
+exports.EVENTS = {
+    UPDATE: `${exports.ADDON_ID}/update`,
+};
+//# sourceMappingURL=constants.js.map
